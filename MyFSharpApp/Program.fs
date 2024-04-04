d81bdc41cbf305646ebb4830ce7842af7aa2e3e6
Ankit <Ankitkumar120503@gmail.com>
2024-04-04 15:33:28 -0400

    v3

---
+++ MyFSharpApp/Program.fs
@@ -0,0 +1,52 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hi Myself Ankit"
+
+type Genre =
+    | Horror
+    | Drama
+    | Thriller
+    | Comedy
+    | Fantasy
+    | Sport
+
+type Director = {
+    Name: string
+    Movies: int
+}
+
+type Movie = {
+    Name: string
+    RunLength: int
+    Genre: Genre
+    Director: Director
+    IMDBRating: float
+}
+
+let MovieDuration totalMinutes =
+    let hours = totalMinutes / 60
+    let remainingMinutes = totalMinutes % 60
+    sprintf "%dh %dmin" hours remainingMinutes
+
+let movies = [
+    { Name = "CODA"; RunLength = 111; Genre = Drama; Director = { Name = "Sian Heder"; Movies = 9 }; IMDBRating = 8.1 }
+    { Name = "Belfast"; RunLength = 98; Genre = Comedy; Director = { Name = "Kenneth Branagh"; Movies = 23 }; IMDBRating = 7.3 }
+    { Name = "Don't Look Up"; RunLength = 138; Genre = Comedy; Director = { Name = "Adam McKay"; Movies = 27 }; IMDBRating = 7.2 }
+    { Name = "Drive My Car"; RunLength = 179; Genre = Drama; Director = { Name = "Ryusuke Hamaguchi"; Movies = 24 }; IMDBRating = 7.6 }
+    { Name = "Dune"; RunLength = 155; Genre = Fantasy; Director = { Name = "Denis Villeneuve"; Movies = 24 }; IMDBRating = 8.1 }
+    { Name = "King Richard"; RunLength = 144; Genre = Sport; Director = { Name = "Reinaldo Marcus Green"; Movies = 15 }; IMDBRating = 7.5 }
+    { Name = "Licorice Pizza"; RunLength = 133; Genre = Comedy; Director = { Name = "Paul Thomas Anderson"; Movies = 49 }; IMDBRating = 7.4 }
+    { Name = "Nightmare Alley"; RunLength = 150; Genre = Thriller; Director = { Name = "Guillermo Del Toro"; Movies = 22 }; IMDBRating = 7.1 }
+]
+
+let oscarWinners = movies |> List.filter (fun movie -> movie.IMDBRating > 7.4)
+
+printfn "All Movies:"
+movies |> List.iter (fun movie -> printfn "%s" movie.Name)
+
+
+printfn "\nOscar Winners:"
+oscarWinners |> List.iter (fun movie -> printfn "%s" movie.Name)
+
+printfn "\nOscar Winners Durations:"
+oscarWinners |> List.iter (fun movie -> printfn "%s" (MovieDuration movie.RunLength))
+
